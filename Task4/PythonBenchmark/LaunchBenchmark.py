db7cacc6f38c47db8c71a8e38b63f60261340049
FilippMaksimov <fil.maksimov.1998@bk.ru>
2023-04-18 04:25:50 +0400

    Task4 file generation and file read added

--- Task4/PythonBenchmark/LaunchBenchmark.py
+++ Task4/PythonBenchmark/LaunchBenchmark.py
@@ -1,16 +1,19 @@
 import time
 
+from numpy.compat import long
+
 from Task4.PythonBenchmark import Benchmark as bench
 
 
 def file_read():
-    with open("variable_data.bin", "rb") as f:
+    with open("../Data/variable_data.bin", "rb") as f:
         s = f.readline()
         row_a = int(s)
         s = f.readline()
         column_a = int(s)
         matrix_a_float = [[0.00 for column in range(column_a)] for row in range(row_a)]
         matrix_a_int = [[0 for column in range(column_a)] for row in range(row_a)]
+        matrix_a_long = [[long(0) for column in range(column_a)] for row in range(row_a)]
         for row in range(row_a):
             for column in range(column_a):
                 bs = f.readline()
@@ -18,6 +21,7 @@ def file_read():
                 s = s[:-1]
                 matrix_a_float[row][column] = float(s)
                 matrix_a_int[row][column] = int(s)
+                matrix_a_long[row][column] = long(s)
 
         empty_line = f.readline()
         s = f.readline()
@@ -26,6 +30,7 @@ def file_read():
         column_b = int(s)
         matrix_b_float = [[0.00 for column in range(column_b)] for row in range(row_b)]
         matrix_b_int = [[0 for column in range(column_b)] for row in range(row_b)]
+        matrix_b_long = [[long(0) for column in range(column_b)] for row in range(row_b)]
         for row in range(row_b):
             for column in range(column_b):
                 bs = f.readline()
@@ -33,40 +38,109 @@ def file_read():
                 s = s[:-1]
                 matrix_b_float[row][column] = float(s)
                 matrix_b_int[row][column] = int(s)
+                matrix_b_long[row][column] = long(s)
 
         empty_line_2 = f.readline()
         alpha_bs = f.readline()
         alpha_s = alpha_bs.decode()
         alpha_float = float(alpha_s)
         alpha_int = int(alpha_s)
+        alpha_long = long(alpha_s)
 
         beta_bs = f.readline()
         beta_s = beta_bs.decode()
         beta_float = float(beta_s)
         beta_int = int(beta_s)
-        return matrix_a_int, matrix_b_int, alpha_int, beta_int, matrix_a_float, matrix_b_float, alpha_float, beta_float
+        beta_long = long(beta_s)
+        return matrix_a_int, matrix_b_int, alpha_int, beta_int, matrix_a_float, \
+            matrix_b_float, alpha_float, beta_float, matrix_a_long, matrix_b_long, alpha_long, beta_long
 
 
 def launch_bench():
+    # For Integers
     matrix_a_int = file_read()[0]
     matrix_b_int = file_read()[1]
     alpha_int = file_read()[2]
     beta_int = file_read()[3]
     data_list_int = []
-    time_start_int = time.time()
-    python_res_int = bench.dgemm_operation(matrix_a_int, matrix_b_int, alpha_int, beta_int)
-    time_end_int = time.time()
+    operation_time = 0
+    n = 0
+    while n != 10:
+        time_start_int = time.time()
+        python_res_int = bench.dgemm_operation(matrix_a_int, matrix_b_int, alpha_int, beta_int)
+        operation_time += int((time.time() - time_start_int) * 10 ** 6)
+        n = n + 1
+    operation_time = int(operation_time / 10)
+    items_a = len(matrix_a_int) * len(matrix_a_int[0])
+    items_b = len(matrix_b_int) * len(matrix_b_int[0])
+    total_sum = items_a + items_b
+    data_list_int.append(operation_time)
+    data_list_int.append(total_sum)
 
+    # For floats(doubles)
     matrix_a_float = file_read()[4]
     matrix_b_float = file_read()[5]
     alpha_float = file_read()[6]
     beta_float = file_read()[7]
     data_list_float = []
-    time_start_int = time.time()
-    python_res_float = bench.dgemm_operation(matrix_a_float, matrix_b_float, alpha_float, beta_float)
-    time_end_float = time.time()
-    return python_res_int, python_res_float
+    operation_time = 0
+    n = 0
+    while n != 10:
+        time_start_float = time.time()
+        python_res_float = bench.dgemm_operation(matrix_a_float, matrix_b_float, alpha_float, beta_float)
+        operation_time += int((time.time() - time_start_float) * 10 ** 6)
+        n = n + 1
+    operation_time = int(operation_time / 10)
+    data_list_float.append(operation_time)
+    data_list_float.append(total_sum)
+
+    # For BigIntegers(long in Python)
+    matrix_a_long = file_read()[8]
+    matrix_b_long = file_read()[9]
+    alpha_long = file_read()[10]
+    beta_long = file_read()[11]
+    data_list_long = []
+    operation_time = 0
+    n = 0
+    while n != 10:
+        time_start_long = time.time()
+        python_res_long = bench.dgemm_operation(matrix_a_long, matrix_b_long, alpha_long, beta_long)
+        operation_time += int((time.time() - time_start_long) * 10 ** 6)
+        n = n + 1
+    operation_time = int(operation_time / 10)
+    data_list_long.append(operation_time)
+    data_list_long.append(total_sum)
+
+    return data_list_int, data_list_float, data_list_long
 
 
-print(launch_bench().__getitem__(0))
-print(launch_bench().__getitem__(1))
+def write_data(threats, data_int, data_long, data_float):
+    try:
+        with open(f"python_threads{threats}.bin", "wb") as f:
+            title_int = "Python integer Data" + '\n'
+            bt = title_int.encode()
+            f.write(bt)
+            for item in data_int:
+                s = str(item) + '\n'
+                bs = s.encode()
+                f.write(bs)
+            title_long = "Python long Data" + '\n'
+            bt = title_long.encode()
+            f.write(bt)
+            for item in data_long:
+                s = str(item) + '\n'
+                bs = s.encode()
+                f.write(bs)
+            title_float = "Python float(double) Data" + '\n'
+            bt = title_float.encode()
+            f.write(bt)
+            for item in data_float:
+                s = str(item) + '\n'
+                bs = s.encode()
+                f.write(bs)
+        print("Файлы успешно записаны")
+    except IOError:
+        print("Возникла ошибка. Файлы не удалось записать")
+
+
+write_data(1, launch_bench()[0], launch_bench()[2], launch_bench()[1])
